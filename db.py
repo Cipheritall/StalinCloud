d0b36db475dc88c7a08bccbf5d54148b6db630ee
Cipher <cipher@10.10>
2023-11-14 18:54:35 +0100

    db

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from config import CONFIG
 
 DATABASE_FILE = "./db/photos.db"
 
@@ -9,6 +10,7 @@ def create_database():
         """
         CREATE TABLE IF NOT EXISTS photos (
             id TEXT PRIMARY KEY,
+            owner TEXT,
             filename TEXT,
             created_time TEXT,
             downloaded_time TEXT,
@@ -19,20 +21,40 @@ def create_database():
     conn.commit()
     conn.close()
 
-def save_to_database(photo_info):
-    conn = sqlite3.connect(DATABASE_FILE)
-    cursor = conn.cursor()
-    cursor.execute(
-        """
-        INSERT OR REPLACE INTO photos (id, filename, created_time, download_url)
-        VALUES (?, ?, ?, ?)
-        """,
-        (
+def add_new_photo(photo_info):
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT OR REPLACE INTO photos (id, owner, filename, created_time, downloaded_time, download_url)
+            VALUES (?, ?, ?, ?, ?, ?)
+        ''', (
             photo_info["id"],
+            CONFIG['owner'],
             photo_info["filename"],
             photo_info["created_time"],
+            photo_info.get("downloaded_time", None),
             photo_info["download_url"],
-        ),
-    )
-    conn.commit()
-    conn.close()
+        ))
+        conn.commit()
+
+def is_photo_in_db(photo_id):
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT COUNT(*) FROM photos WHERE id = ?', (photo_id,))
+        return cursor.fetchone()[0] > 0
+
+def is_photo_downloaded(photo_id):
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT COUNT(*) FROM photos WHERE id = ? AND downloaded_time IS NOT NULL', (photo_id,))
+        return cursor.fetchone()[0] > 0
+
+def mark_as_downloaded(photo_id, downloaded_time):
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            UPDATE photos
+            SET downloaded_time = ?
+            WHERE id = ?
+        ''', (downloaded_time, photo_id))
+        conn.commit()
