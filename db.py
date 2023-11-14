442eddeca40ae2e44535214483cf0f4f0bb58e09
Cipher <cipher@10.10>
2023-11-15 00:03:19 +0100

    google id db

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import sqlite3
 from config import CONFIG
+import datetime
 
 DATABASE_FILE = "./db/photos.db"
 
@@ -9,52 +10,65 @@ def create_database():
     cursor.execute(
         """
         CREATE TABLE IF NOT EXISTS photos (
-            id TEXT PRIMARY KEY,
+            id TEXT PRIMARY KEY AUTOINCREMENT,
+            google_id TEXT,
+            album TEXT,
             owner TEXT,
             filename TEXT,
             created_time TEXT,
             downloaded_time TEXT,
-            download_url TEXT
+            local_path TEXT,
+            productUrl TEXT,
+            baseUrl TEXT,
+            mimeType TEXT,
         )
         """
     )
     conn.commit()
     conn.close()
 
+
 def add_new_photo(photo_info):
     with sqlite3.connect(DATABASE_FILE) as conn:
         cursor = conn.cursor()
         cursor.execute('''
-            INSERT OR REPLACE INTO photos (id, owner, filename, created_time, downloaded_time, download_url)
-            VALUES (?, ?, ?, ?, ?, ?)
+            INSERT OR REPLACE INTO photos (
+                google_id, album, owner, filename, created_time,
+                downloaded_time, local_path, productUrl, baseUrl, mimeType
+            ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ''', (
-            photo_info["id"],
-            CONFIG['owner'],
-            photo_info["filename"],
-            photo_info["created_time"],
+            photo_info.get("google_id", None),
+            photo_info.get("album", None),
+            photo_info.get("owner", None),
+            photo_info.get("filename", None),
+            photo_info.get("created_time", None),
             photo_info.get("downloaded_time", None),
-            photo_info["download_url"],
+            photo_info.get("local_path", None),
+            photo_info.get("productUrl", None),
+            photo_info.get("baseUrl", None),
+            photo_info.get("mimeType", None),
         ))
         conn.commit()
 
-def is_photo_in_db(photo_id):
+def is_photo_in_db(google_id):
     with sqlite3.connect(DATABASE_FILE) as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT COUNT(*) FROM photos WHERE id = ?', (photo_id,))
+        cursor.execute('SELECT COUNT(*) FROM photos WHERE google_id = ?', (google_id,))
         return cursor.fetchone()[0] > 0
 
-def is_photo_downloaded(photo_id):
+def is_photo_downloaded(google_id):
     with sqlite3.connect(DATABASE_FILE) as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT COUNT(*) FROM photos WHERE id = ? AND downloaded_time IS NOT NULL', (photo_id,))
+        cursor.execute('SELECT COUNT(*) FROM photos WHERE google_id = ? AND downloaded_time IS NOT NULL', (google_id,))
         return cursor.fetchone()[0] > 0
 
-def mark_as_downloaded(photo_id, downloaded_time):
+def mark_as_downloaded(google_id, local_path):
+    downloaded_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with sqlite3.connect(DATABASE_FILE) as conn:
         cursor = conn.cursor()
         cursor.execute('''
             UPDATE photos
-            SET downloaded_time = ?
-            WHERE id = ?
-        ''', (downloaded_time, photo_id))
+            SET downloaded_time = ?, local_path = ?
+            WHERE google_id = ?
+        ''', (downloaded_time, local_path, google_id))
         conn.commit()
