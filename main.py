73ee31541b19895125b0c7e138631d39d08f7171
Cipher <cipher@10.10>
2023-11-15 20:48:41 +0100

    args

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import os
-from Google import Create_Service,sync_photos
+import Google
 import logging
 import db
 from config import CONFIG
-logging.basicConfig(filename="log/main.log", level=logging.DEBUG)
+import files
+import argparse
+import api
 
 
-db.create_photos_database()
-service = Create_Service()
-sync_photos(service,target_num=100)
+if __name__ == "__main__":
+    # Configure the logging module
+    logging.basicConfig(filename="log/main.log", level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    parser = argparse.ArgumentParser(description="StalinCloud v0.1")
+    # Add command-line arguments
+    parser.add_argument("--sync", help="Number of new assets to sync")
+    parser.add_argument("--xx", help="xx")
+    # Add more arguments as needed
+    parser.add_argument("--move", action="store_true", help="Move files to a new directory defined in config.yml [media_path]")
+
+    args = parser.parse_args()
+
+    if args.sync:
+        db.create_photos_database()
+        service = Google.Create_Service()
+
+        Google.sync_photos(service,target_num=100)
+
+    if args.move:
+        api.move_all_photos()
